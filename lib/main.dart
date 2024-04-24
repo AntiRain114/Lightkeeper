bb76f01995746b06d0232e7083c75ca7262cc8c7
AntiRain114 <zhongjiezhe1973@hotmail.com>
2024-04-24 11:36:17 +0100

    new

--- lib/main.dart
+++ lib/main.dart
@@ -1,125 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:camera/camera.dart';
+import 'package:flutter_vision/flutter_vision.dart';
+import 'dart:typed_data';
 
-void main() {
-  runApp(const MyApp());
+class CameraScreen extends StatefulWidget {
+  @override
+  _CameraScreenState createState() => _CameraScreenState();
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+class _CameraScreenState extends State<CameraScreen> {
+  CameraController? _controller;
+  List<CameraDescription>? _cameras;
+  bool _isDetecting = false;
+  int _modelOutput = 0;
+  late FlutterVision vision;
 
-  // This widget is the root of your application.
+  @override
+  void initState() {
+    super.initState();
+    _initializeCamera();
+    _initializeVision();
+  }
+
+  Future<void> _initializeCamera() async {
+    _cameras = await availableCameras();
+    _controller = CameraController(_cameras![0], ResolutionPreset.medium);
+    await _controller!.initialize();
+    setState(() {});
+  }
+
+  Future<void> _initializeVision() async {
+    vision = FlutterVision();
+    await vision.loadModel('assets/gestureModel_one.tflite');
+  }
+
+  Future<void> _detectGesture(CameraImage image) async {
+    if (!_isDetecting) {
+      _isDetecting = true;
+
+      int startX = (image.width * 0.2).toInt();
+      int endX = (image.width * 0.8).toInt();
+      int startY = (image.height * 0.2).toInt();
+      int endY = (image.height * 0.8).toInt();
+
+      int rowStride = image.planes[0].bytesPerRow;
+      int pixelStride = image.planes[0].bytesPerPixel!;
+
+      int size = (endX - startX) * (endY - startY);
+      var img = Uint8List(size);
+
+      int index = 0;
+      for (int y = startY; y < endY; y++) {
+        for (int x = startX; x < endX; x++) {
+          int pixelIndex = y * rowStride + x * pixelStride;
+          img[index++] = image.planes[0].bytes[pixelIndex];
+        }
+      }
+
+      img = _preprocessImage(img);
+
+      var output = await vision.runModel(img);
+
+      setState(() {
+        _modelOutput = output[0].toInt();
+      });
+
+      _isDetecting = false;
+    }
+  }
+
+  Uint8List _preprocessImage(Uint8List img) {
+    int size = 100;
+    var output = Uint8List(size * size);
+
+    int rowStride = (img.length / size).round();
+    for (int x = 0; x < size; x++) {
+      for (int y = 0; y < size; y++) {
+        int pixelIndex = y * rowStride + x;
+        output[y * size + x] = img[pixelIndex];
+      }
+    }
+
+    return output;
+  }
+
   @override
   Widget build(BuildContext context) {
+    if (_controller == null || !_controller!.value.isInitialized) {
+      return Container();
+    }
+
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // TRY THIS: Try running your application with "flutter run". You'll see
-        // the application has a purple toolbar. Then, without quitting the app,
-        // try changing the seedColor in the colorScheme below to Colors.green
-        // and then invoke "hot reload" (save your changes or press the "hot
-        // reload" button in a Flutter-supported IDE, or press "r" if you used
-        // the command line to start the app).
-        //
-        // Notice that the counter didn't reset back to zero; the application
-        // state is not lost during the reload. To reset the state, use hot
-        // restart instead.
-        //
-        // This works for code too, not just values: Most code changes can be
-        // tested with just a hot reload.
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // TRY THIS: Try changing the color here to a specific color (to
-        // Colors.amber, perhaps?) and trigger a hot reload to see the AppBar
-        // change color while the other colors stay the same.
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          //
-          // TRY THIS: Invoke "debug painting" (choose the "Toggle Debug Paint"
-          // action in the IDE, or press "p" in the console), to see the
-          // wireframe for each widget.
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+      home: Scaffold(
+        appBar: AppBar(title: Text('Gesture Recognition')),
+        body: Column(
+          children: [
+            Expanded(
+              child: CameraPreview(_controller!),
             ),
             Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
+              'Predicted Gesture: $_modelOutput',
+              style: TextStyle(fontSize: 20),
             ),
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
     );
+  }
+
+  @override
+  void dispose() {
+    _controller?.dispose();
+    vision.close();
+    super.dispose();
   }
 }
